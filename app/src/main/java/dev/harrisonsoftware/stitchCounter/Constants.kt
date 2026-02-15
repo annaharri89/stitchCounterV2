fda1354b59872de29736ed3a96934613fd8adf6e
Anna Harrison <16767360+annaharri89@users.noreply.github.com>
2026-02-14 22:59:54 -0500

    fixed the kofi link

--- app/src/main/java/dev/harrisonsoftware/stitchCounter/Constants.kt
+++ app/src/main/java/dev/harrisonsoftware/stitchCounter/Constants.kt
@@ -3,7 +3,7 @@ package dev.harrisonsoftware.stitchCounter
 object Constants {
     const val PRIVACY_POLICY_URL = "https://harrisonsoftware.dev/stitch-counter-v2/privacy-policy"
     const val EULA_URL = "https://harrisonsoftware.dev/stitch-counter-v2/eula"
-    const val KOFI_URL = "https://ko-fi.com/stitchcounter"
+    const val KOFI_URL = "https://ko-fi.com/annaharri"
     const val SUPPORT_EMAIL = "support@harrisonsoftware.dev"
     const val BUG_REPORT_SUBJECT = "Stitch Counter - Bug Report"
     const val FEEDBACK_SUBJECT = "Stitch Counter - Feedback"
